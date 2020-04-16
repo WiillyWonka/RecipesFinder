5ed5afbf4452d18fdc34917ca99e6b6271dc2601
YudzhinNSK <49250312+YudzhinNSK@users.noreply.github.com>
2020-04-16 20:13:28 +0300

    #80 update window with favorite recipes

--- app/src/main/java/com/example/recepiesfinder/MainActivity.java
+++ app/src/main/java/com/example/recepiesfinder/MainActivity.java
@@ -11,26 +11,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button AddIngrBt;
-    Button AllRecBt;
+    ImageButton AddIngrBt;
+    ImageButton AllRecBt;
+    ImageButton FavoritesBt;
+    ImageButton MeRecBt;
+    Dish dish;
+    DataBase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AddIngrBt = (Button) findViewById(R.id.AddIngr);
-        AllRecBt = (Button) findViewById(R.id.Allrec);
+        AddIngrBt = (ImageButton) findViewById(R.id.add_ingr);
+        AllRecBt = (ImageButton) findViewById(R.id.all_recipes);
+        FavoritesBt = (ImageButton) findViewById(R.id.favorites);
+        MeRecBt = (ImageButton) findViewById(R.id.my_recipes);
 
-        ;
+        db = DataBase.getDataBase(this);
+        dish = db.getDishById(1);
 
         AddIngrBt.setOnClickListener(this);
         AllRecBt.setOnClickListener(this);
+        FavoritesBt.setOnClickListener(this);
+        MeRecBt.setOnClickListener(this);
     }
 
     @Override
@@ -59,14 +69,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.AddIngr:
-                Intent intent1 = new Intent(this, FindIngredient.class);
+            case R.id.add_ingr:
+                Intent intent2 = new Intent(this, FindIngredient.class);
+                startActivity(intent2);
+                break;
+            case R.id.all_recipes:
+                Intent intent1 = new Intent(this, DisplayListOfDishesActivity.class);
+                intent1.putExtra("class_name", MainActivity.class.getName());
                 startActivity(intent1);
                 break;
-            case R.id.Allrec:
+            case R.id.favorites:
                 Intent intent3 = new Intent(this, DisplayListOfDishesActivity.class);
-                intent3.putExtra("class_name", MainActivity.class.getName());
+                intent3.putExtra("class_name", "favourites");
                 startActivity(intent3);
+                break;
+            case R.id.my_recipes:
                 break;
         }
     }
